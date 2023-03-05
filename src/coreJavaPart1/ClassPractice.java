eb9e15b2213ed7b027e8ec64e735600ef84ac6f1
Kaniz Fatema <fatemakaniz60@gmail.com>
2023-03-05 01:07:41 -0500

    technical test #1

---
+++ src/coreJavaPart1/ClassPractice.java
@@ -0,0 +1,9 @@
+package coreJavaPart1;
+
+public class ClassPractice {
+    int myNumber=500;
+    public static void main (String [] args){
+        System.out.println("Hello Ts4U Automation");
+
+    }
+}
