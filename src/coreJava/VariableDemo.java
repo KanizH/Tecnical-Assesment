b80bb07c2c9814b3d949e06fef12d5ba826503b2
Kaniz Fatema <fatemakaniz60@gmail.com>
2023-03-07 13:11:10 -0500

    technical test #3

---
+++ src/coreJava/VariableDemo.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public class VariableDemo {
+
+    public static void main(String[] args) {
+        //Local variables
+        String firstName = "Kaniz";
+        String lastName = "Fatema";
+
+        System.out.println(firstName);
+
+        System.out.println(lastName);
+
+    }
+}
