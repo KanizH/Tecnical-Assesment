6ff95795bf289f4012c936ca18fcd0d124f131ec
Kaniz Fatema <fatemakaniz60@gmail.com>
2023-03-07 12:58:50 -0500

    technical test #2

--- src/coreJava/ClassPractice.java
+++ src/coreJava/ClassPractice.java
@@ -1,5 +1,5 @@
-package coreJavaPart1;
-
+package coreJava;
+//Technical test #2 Update the name of the package and class name.
 public class ClassPractice {
     int myNumber=500;
     public static void main (String [] args){
